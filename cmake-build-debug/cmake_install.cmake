865fc6edd308f2009a9d533e4eb2dc3d457443ee
1Bc <mycodeisx@gmail.com>
2024-11-26 15:38:08 +0100

    rename project

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Philip/Documents/PWr/7_semestr/ZPI/sensor_unit_L0/sensor_unit
+# Install script for directory: C:/Users/Philip/Documents/PWr/Studia/Sem 7/ZPI/sensor_unit
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/sensor_unit")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/firmware-probe")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/Philip/Documents/PWr/7_semestr/ZPI/sensor_unit_L0/sensor_unit/cmake-build-debug/External/cmake_install.cmake")
+  include("C:/Users/Philip/Documents/PWr/Studia/Sem 7/ZPI/sensor_unit/cmake-build-debug/External/cmake_install.cmake")
 
 endif()
 
@@ -58,6 +58,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Philip/Documents/PWr/7_semestr/ZPI/sensor_unit_L0/sensor_unit/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Philip/Documents/PWr/Studia/Sem 7/ZPI/sensor_unit/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
